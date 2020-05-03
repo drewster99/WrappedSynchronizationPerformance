8a6cc3195d5b62f6d977c178f38335da73ac2eed
Andrew Benson <drewbenson@netjack.com>
2020-05-03 00:15:06 -0500

    Initial Commit

---
+++ PerformanceDispatchQueueSync/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PerformanceDispatchQueueSync
+//
+//  Created by Andrew Benson on 5/3/20.
+//  Copyright © 2020 Nuclear Cyborg Corp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
